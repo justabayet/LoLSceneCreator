2bb398272c04ef5ee633277c88fa63041f91cf40
justabayet <anthony.bayet@hotmail.com>
2023-12-02 00:30:52 +0100

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,47 @@
-const path = require('path');
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    entry: './src/empty.ts',
-    output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: 'main.js'
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: "assets", to: "public/assets" },
-                { from: "lib", to: "public/lib" },
-            ],
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'public'),
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "assets", to: "assets" },
+        { from: "lib", to: "lib" },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
-      port: 9000,
-      hot: true,
-    }
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 9000,
+    hot: true,
+  },
 };
