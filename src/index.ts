2e9f3fd9c23877d9a5e274e1282ff2d6a8362fab
justabayet <anthony.bayet@hotmail.com>
2023-12-02 21:26:10 +0100

    Call clash data endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 import './style.css'
 import './libIndex.js'
+
+const CLASH_ENDPOINT = 'https://whenisnextlolclash.justabayet.com/clashes'
+
+async function main (): Promise<void> {
+  const remoteData = await fetch(CLASH_ENDPOINT)
+  console.log(await remoteData.json())
+  const mockData = (await import('./data/clashMock.json')).default
+  console.log(mockData)
+}
+
+void main()
