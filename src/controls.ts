a7d659a2bb56ba98007205dfd1ddc560f74f71a5
justabayet <anthony.bayet@hotmail.com>
2023-12-05 00:21:34 +0100

    - Controls

--- src/controls.ts
+++ src/controls.ts
@@ -1,25 +1,9 @@
-import { state } from './state'
-
 function setupControls (): void {
   window.addEventListener('keydown', onKeyDown, false)
 }
 
 function onKeyDown (event: KeyboardEvent): void {
-  if (event.key === 'q') {
-    state.champions.forEach((champion) => champion.mesh?.userData.model.setAnimationOnce('spell1'))
-  }
-  if (event.key === 'w') {
-    state.champions.forEach((champion) => champion.mesh?.userData.model.setAnimationOnce('spell2'))
-  }
-  if (event.key === 'e') {
-    state.champions.forEach((champion) => champion.mesh?.userData.model.setAnimationOnce('spell3'))
-  }
-  if (event.key === 'r') {
-    state.champions.forEach((champion) => champion.mesh?.userData.model.setAnimationOnce('spell4'))
-  }
-  if (event.key === 'a') {
-    state.champions.forEach((champion) => champion.mesh?.userData.model.setAnimationOnce('attack1'))
-  }
+  // console.log(`Key stroke: ${event.key}`)
 }
 
 export { setupControls }
