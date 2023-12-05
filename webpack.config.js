22a733c9819266a5de1aa641ca8e8037c98471d8
justabayet <anthony.bayet@hotmail.com>
2023-12-05 12:53:22 +0100

    # Move CNAME

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   plugins: [
     new CopyPlugin({
       patterns: [{ from: "assets", to: "assets" }],
+      patterns: [{ from: "CNAME" }],
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
